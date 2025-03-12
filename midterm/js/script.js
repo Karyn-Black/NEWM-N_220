7f36d8da500623a74940c8e07764430e4df816eb
Karyn-Black <caringkaryn5@gmail.com>
2025-03-12 15:52:16 -0400

    Updated Menu Item items

--- midterm/js/script.js
+++ midterm/js/script.js
@@ -1,3 +1,14 @@
 let = menu;
 let = cart;
-let = no - items;
+let = no_items;
+
+const food = [
+  { name: "Hamburger", price: "$2.99" },
+  { name: "Cheeseburger", price: "$3.99" },
+  { name: "Cheese Pizza", price: "$3.99" },
+  { name: "Pepperoni Pizza", price: "$4.99" },
+  { name: "Sausage Pizza", price: "$4.99" },
+  { name: "Caesar Salad", price: "$3.99" },
+  { name: "Fries", price: "$1.99" },
+  { name: "Onion Rings", price: "$2.49" },
+];
