6d77be098b1891e590a2e6b18e180924dee2d40a
Karyn-Black <caringkaryn5@gmail.com>
2025-05-06 05:19:46 -0400

    Re-watching the Week 13 class video and following along

--- homework-12/js/api.js
+++ homework-12/js/api.js
@@ -1,22 +1,27 @@
 // https://digimon-api.vercel.app/api/digimon
 
 function getDigimon() {
-fetch("https://digimon-api.vercel.app/api/digimon").then(function (digiResp) {
-    return digiResp,json ();
-  });
-  .then((digiData) => {
-    console.log(digiData);
-  })
-  .catch(function (err) {
-    console.warn(err);
-  })
-  .finally(() => {
-    console.log("Fetch complete!");
-    // Code to cleanup => do it here
-  })};
+  fetch("https://digimon-api.vercel.app/api/digimon")
+    .then(function (digiResp) {
+      console.log(digiResp);
+      return digiResp.json();
+    })
+    .then((digiData) => {
+      console.log(digiData);
+    })
+    .catch(function (err) {
+      console.warn(err);
+    })
+    .finally(() => {
+      console.log("Fetch complete!");
+      // Code to cleanup => do it here
+    });
+}
 
-const digiResp = await fetch ("https://digimon-api.vercel.app/api/digimon");
+getDigimon();
 
-const digiData = await digiResp.json();
+// const digiResp = await fetch ("https://digimon-api.vercel.app/api/digimon");
 
-console.log(digiData);
+// const digiData = await digiResp.json();
+
+// console.log(digiData);
