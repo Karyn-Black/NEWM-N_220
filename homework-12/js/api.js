fe3a15786ae6d2774467475e76c771517274329a
Karyn-Black <caringkaryn5@gmail.com>
2025-05-07 23:05:21 -0400

    Am at time stamp 1:05:00 of the week 13 video

--- homework-12/js/api.js
+++ homework-12/js/api.js
@@ -1,27 +1,33 @@
 // https://digimon-api.vercel.app/api/digimon
 
-function getDigimon() {
-  fetch("https://digimon-api.vercel.app/api/digimon")
-    .then(function (digiResp) {
-      console.log(digiResp);
-      return digiResp.json();
-    })
-    .then((digiData) => {
-      console.log(digiData);
-    })
-    .catch(function (err) {
-      console.warn(err);
-    })
-    .finally(() => {
-      console.log("Fetch complete!");
-      // Code to cleanup => do it here
-    });
+async function getDigimon() {
+  // fetch("https://digimon-api.vercel.app/api/digimon")
+  //   .then(function (digiResp) {
+  //     console.log(digiResp);
+  //     return digiResp.json();
+  //   })
+  //   .then((digiData) => {
+  //     console.log(digiData);
+  //   })
+  //   .catch(function (err) {
+  //     console.warn(err);
+  //   })
+  //   .finally(() => {
+  //     console.log("Fetch complete!");
+  //     // Code to cleanup => do it here
+  //   });
+
+  try {
+    const digiResp = await fetch("https://digimon-api.vercel.app/api/digimons");
+
+    const digiData = await digiResp.json();
+
+    console.log(digiData);
+
+    console.log("End of function");
+  } catch (err) {
+    console.warn(err);
+  }
 }
 
 getDigimon();
-
-// const digiResp = await fetch ("https://digimon-api.vercel.app/api/digimon");
-
-// const digiData = await digiResp.json();
-
-// console.log(digiData);
